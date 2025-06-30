dad5326c534717936d8f85c8b85b01801e97585d
sofiamellano <mellanosofia1@gamail.com>
2025-06-30 15:07:24 -0300

    Refactorización de servicios y cambios en estilos
    Se ha cambiado la inyección de `IClienteService` a `IGenericService<Cliente>` en varios archivos, lo que sugiere un enfoque más genérico para la gestión de servicios.
    
    Además, se ha modificado la llamada a `GetAllAsync()` para aceptar un parámetro de cadena vacío en los métodos `OnInitializedAsync`, mejorando la obtención de datos sin filtros.
    
    Se han eliminado las inyecciones de `IClienteService` y `ILocalidadService` en `Program.cs`, y se ha actualizado el nombre del archivo de estilos en `index.html` de `KioscoInformaticoWeb.styles.css` a `Web.styles.css`.

--- KioscoInformaticoWeb/Program.cs
+++ KioscoInformaticoWeb/Program.cs
@@ -13,8 +13,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
-builder.Services.AddScoped<IClienteService, ClienteService>();
-builder.Services.AddScoped<ILocalidadService, LocalidadService>();
 builder.Services.AddScoped<FirebaseAuthService>();
 builder.Services.AddSweetAlert2();
 
